af73c450557447ff7b27f3c49e746f6e29762a6e
henry neo <neohenry@gmail.com>
2022-08-23 15:38:23 +0800

    pending work on book detail

--- src/app/components/book-detail/book-detail.component.ts
+++ src/app/components/book-detail/book-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Book } from '../../book';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { BookService } from '../../book.service';
 
 @Component({
   selector: 'app-book-detail',
@@ -9,7 +12,18 @@ import { Book } from '../../book';
 export class BookDetailComponent implements OnInit {
   @Input() book?: Book;
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private bookService: BookService,
+    private location: Location
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getBook();
+  }
+
+  getBook(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    console.log('Our book detail id:', id);
+  }
 }
--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -9,7 +9,7 @@ import { BookService } from '../../book.service';
 })
 export class BooksComponent implements OnInit {
   books: Book[] = [];
-  selectedBook?: Book;
+  // selectedBook?: Book;
 
   constructor(private bookService: BookService) {}
 
@@ -17,9 +17,9 @@ export class BooksComponent implements OnInit {
     this.getBooks();
   }
 
-  onSelect(book: Book): void {
-    this.selectedBook = book;
-  }
+  // onSelect(book: Book): void {
+  //   this.selectedBook = book;
+  // }
 
   getBooks(): void {
     this.books = this.bookService.getBooks();
