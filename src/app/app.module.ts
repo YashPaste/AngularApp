727ee9a46749eae2debd21c0f8a84aa10d288109
henry neo <neohenry@gmail.com>
2022-08-23 15:36:22 +0800

    add routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './components/books/books.component';
 import { BookDetailComponent } from './components/book-detail/book-detail.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, BooksComponent, BookDetailComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, BooksComponent, BookDetailComponent, AboutComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
